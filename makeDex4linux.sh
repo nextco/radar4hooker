eb8c52bf97460c716060bf830a9b0c36022c5a56
stephen <>
2026-03-18 14:46:52 +0800

    Move jars into libs and update build config

--- makeDex4linux.sh
+++ makeDex4linux.sh
@@ -1,11 +1,11 @@
 rm -rf classes/
 mkdir classes
 find . -name "*.java">sources.txt
-javac -encoding utf-8 -Xlint:unchecked  -classpath android.jar:android-support-v4.jar:nanohttpd-2.3.1.jar:fastjson-1.2.9.jar:org.apache.http.legacy.jar:android-support-v7-recyclerview.jar:androidx-appcompat-1.2.0.jar:androidx-core-1.3.1.jar:androidx-fragment-1.1.0.jar:androidx-activity-1.2.3.jar:androidx-activity-lint-1.2.3.jar:androidx-viewpager-1.0.0.jar:viewpager2-1.1.0-alpha01.jar:recyclerview-1.2.0.jar -d classes @sources.txt
-cp nanohttpd-2.3.1.jar classes/
-cp okhttp-3.12.6.jar classes/
-cp okio-1.15.0.jar classes/
-cp fastjson-1.2.9.jar classes/
+javac -encoding utf-8 -Xlint:unchecked  -classpath libs/android.jar:libs/android-support-v4.jar:libs/nanohttpd-2.3.1.jar:libs/fastjson-1.2.9.jar:libs/org.apache.http.legacy.jar:libs/android-support-v7-recyclerview.jar:libs/androidx-appcompat-1.2.0.jar:libs/androidx-core-1.3.1.jar:libs/androidx-fragment-1.1.0.jar:libs/androidx-activity-1.2.3.jar:libs/androidx-activity-lint-1.2.3.jar:libs/androidx-viewpager-1.0.0.jar:libs/viewpager2-1.1.0-alpha01.jar:libs/recyclerview-1.2.0.jar -d classes @sources.txt
+cp libs/nanohttpd-2.3.1.jar classes/
+cp libs/okhttp-3.12.6.jar classes/
+cp libs/okio-1.15.0.jar classes/
+cp libs/fastjson-1.2.9.jar classes/
 cd classes/
 jar xvf nanohttpd-2.3.1.jar
 jar xvf okhttp-3.12.6.jar
@@ -14,7 +14,7 @@ jar xvf fastjson-1.2.9.jar
 rm -rf *.jar
 jar cvf merge.jar .
 cd ..
-java -jar jarjar-1.3.jar process rule.txt classes/merge.jar classes/xradar.jar
+java -jar libs/jarjar-1.3.jar process rule.txt classes/merge.jar classes/xradar.jar
 rm classes/merge.jar
 #替换你本地的dx路径
 alias dx=$ANDROID_HOME/build-tools/30.0.3/dx
